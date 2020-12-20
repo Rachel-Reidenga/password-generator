d3f4c547f46a0ff2a9c81c3ac4c097024e778775
Rachel-Reidenga <reidenga@gmail.com>
2020-12-20 11:04:25 -0600

    variable values

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 // Assignment code here
 
 // User input variables 
+
+var enter;
+var confirmNumber;
+var confirmChar;
+var confirmLow;
+var confirmUp;
+
+// Variable values
+characters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", ";", ":", "/", "-", "_", "+", ];
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8 ,9 ,0];
+
+lowLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+upLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
 console.log(Math.floor((Math.random() *128) + 1));
 
 
