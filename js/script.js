7febe0e85b42d6ee85095394c67a3654cb34cefb
Rachel-Reidenga <reidenga@gmail.com>
2020-12-20 11:23:27 -0600

    asks for input

--- js/script.js
+++ js/script.js
@@ -11,11 +11,31 @@ var confirmUp;
 // Variable values
 characters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", ";", ":", "/", "-", "_", "+", ];
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8 ,9 ,0];
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8 ,9];
 
 lowLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 upLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// Generate Password
+function generatePassword() {
+  enter = parseInt(prompt("Choose between 8 and 128 characters"));
+}
+
+if (!enter) {
+  alert("Please select number of characters");
+} 
+
+else if (enter < 8 || enter > 128) {
+  enter = parseInt(prompt("Please choose between 8 and 128"));
+}
+
+else {
+  confirmNumber = confirm("Would you like your password to have numbers?");
+  confirmChar = confirm("Would you like your password to have special characters?");
+  confirmLow = confirm("Would you like your password to have lowercase letters?");
+  confirmUp = confirm("Would you like your password to have uppercase letters?");
+}
 
 console.log(Math.floor((Math.random() *128) + 1));
 
