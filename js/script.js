00b513b6d436942356794d6db40154ca819b06c1
Rachel-Reidenga <reidenga@gmail.com>
2020-12-20 20:34:33 -0600

    else confirm

--- js/script.js
+++ js/script.js
@@ -2,20 +2,23 @@
 
 // User input variables 
 
+// Variables
 var enter;
 var confirmNumber;
 var confirmChar;
 var confirmLow;
 var confirmUp;
 
-// Variable values
-characters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", ";", ":", "/", "-", "_", "+", ];
+// Arrays
+var characters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", ";", ":", "/", "-", "_", "+", ];
 
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8 ,9];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8 ,9];
 
-lowLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lowLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-upLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var upLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+console.log(characters, numbers, lowLetters, upLetters.length);
 
 // Generate Password
 function generatePassword() {
@@ -26,28 +29,41 @@ if (!enter) {
   alert("Please select number of characters");
 } 
 
-else if (enter < 8 || enter > 128) {
+else if (enter > 8 || enter < 128) {
   enter = parseInt(prompt("Please choose between 8 and 128"));
+} 
+  
+  else {
+  confirmNumber = confirm("Would you like your password to have numbers?")
+  confirmChar = confirm("Would you like your password to have special characters?")
+  confirmLow = confirm("Would you like your password to have lowercase letters?")
+  confirmUp = confirm("Would you like your password to have uppercase letters?")
 }
 
-else {
-  confirmNumber = confirm("Would you like your password to have numbers?");
-  confirmChar = confirm("Would you like your password to have special characters?");
-  confirmLow = confirm("Would you like your password to have lowercase letters?");
-  confirmUp = confirm("Would you like your password to have uppercase letters?");
-}
 
-//Else if all "false"
+// Else if all "false"
 if (!confirmNumber && !confirmChar && !confirmLow && !confirmUp) {
-  select = alert("You must select password parameters!")
+  choice = alert("You must select password parameters!")
 }
 
-//Else if all "true"
-else if (!confirmNumber && !confirmChar && !confirmLow && !confirmUp) {
+// Else if all "true"
+if (confirmNumber && confirmChar && confirmLow && confirmUp) {
   select = characters.concat(numbers, lowLetters, upLetters);
 }
 
 //Else if 3 "true"
+else if (confirmNumber && confirmChar && confirmLow) {
+  select = characters.concat(numbers, lowLetters,);
+}
+else if (confirmNumber && confirmChar && confirmUp) {
+  select = characters.concat(numbers, upLetters,);
+}
+else if (confirmChar && confirmLow && confirmUp) {
+  select = characters.concat(lowLetters, upLetters);
+}
+else if (confirmNumber && confirmLow && confirmUp) {
+  select = numbers.concat(lowLetters, upLetters);
+}
 
 //Else if 2 "true"
 
